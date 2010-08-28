f5e5a374ec5779e5a50a594f542feb0297b05ba8
Shane Isbell <sisbell@Shane-Isbells-MacBook-Pro.local>
2010-08-28 13:55:00 -0700

    Fixed windows install script. Updated readme to reflect latest changes in modules and build.

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,5 +1,11 @@
-call mvn install:install-file -Dfile=../thirdparty/jaxb/jaxb-api-1.0.1.jar -DgroupId=javax.xml.bind -DartifactId=jaxb-api -Dversion=1.0.1 -Dpackaging=jar
-call mvn install:install-file -Dfile=../thirdparty/jaxb/jaxb-impl-1.0.1.jar -DgroupId=com.sun.xml.bind -DartifactId=jaxb-impl -Dversion=1.0.1 -Dpackaging=jar
-call mvn install:install-file -Dfile=../thirdparty/jaxb/jaxb-libs-1.0.1.jar -DgroupId=com.sun.msv -DartifactId=jaxb-libs -Dversion=1.0.1 -Dpackaging=jar
-call mvn install:install-file -Dfile=../thirdparty/jaxb/namespace-1.0.1.jar -DgroupId=javax.xml.namespace -DartifactId=namespace -Dversion=1.0.1 -Dpackaging=jar
-call mvn install:install-file -Dfile=../thirdparty/jaxb/xsdlib-1.0.1.jar -DgroupId=com.sun.msv -DartifactId=xsdlib -Dversion=1.0.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/commons-email-1.2.jar -DgroupId=org.apache.commons -DartifactId=commons-email -Dversion=1.2 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/javamail/dsn.jar -DgroupId=mail -DartifactId=dsn -Dversion=1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/javamail/imap.jar -DgroupId=mail -DartifactId=imap -Dversion=1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/javamail/mailapi.jar -DgroupId=mail -DartifactId=mailapi -Dversion=1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/javamail/imap.jar -DgroupId=mail -DartifactId=imap -Dversion=1.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=../thirdparty/jaxb-ri-2.1/jaxb-api.jar -DgroupId=javax.xml.bind -DartifactId=jaxb-api -Dversion=2.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/jaxb-ri-2.1/jsr173_1.0_api.jar -DgroupId=javax.xml.bind -DartifactId=jsr173_api -Dversion=1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/jaxb-ri-2.1/activation.jar -DgroupId=javax.xml.bind -DartifactId=activation -Dversion=1.0.2 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/jaxb-ri-2.1/jaxb-impl.jar -DgroupId=com.sun.xml.bind -DartifactId=jaxb-impl -Dversion=2.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=../thirdparty/jaxb-ri-2.1/jaxb-xjc.jar -DgroupId=com.sun.xml.bind -DartifactId=jaxb-xjc -Dversion=2.1 -Dpackaging=jar
